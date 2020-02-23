94b1b6c167cfdf260c40c312868ddd9c8f8b05ea
snowhero <xionghiroyuki@gmail.com>
2020-02-22 17:46:32 -0800

    update: postgres backend

--- app/main/settings.py
+++ app/main/settings.py
@@ -95,7 +95,7 @@ DATABASES = {
         'ENGINE' : 'django.db.backends.postgresql',
         'NAME' : 'goalplanning',
         'USER' : 'postgres',
-        'PASSWORD' : 'Hiro1234Xiong',
+        'PASSWORD' : 'kc05061994hx',
         'HOST' : '127.0.0.1',
         'PORT' : '5432',
    }
